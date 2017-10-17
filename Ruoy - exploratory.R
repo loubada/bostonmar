7510cd895b22bd6e30e41096aaa66cf2f830ca1a
Cyriac96 <cyriacparisot@gmail.com>
2017-10-17 15:52:02 +0200

    Put raw data in tables

--- Ruoy - exploratory.R
+++ Ruoy - exploratory.R
@@ -2,3 +2,4 @@ data_2015 <- read.csv2("marathon_results_2015.csv", sep = ",")
 data_2016 <- read.csv2("marathon_results_2016.csv", sep = ",")
 data_2017 <- read.csv2("marathon_results_2017.csv", sep = ",")
 
+
--- Script_Cyriac.R
+++ Script_Cyriac.R
@@ -1,6 +1,3 @@
 #Script de Cyriac #BG
 #or not
 library(readxl)
-data_2015 <- read.csv2("marathon_results_2015.csv")
-data_2016 <- read.csv2("marathon_results_2016.csv")
-data_2017 <- read.csv2("marathon_results_2017.csv")
