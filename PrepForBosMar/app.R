446ca3d17d058e53edeeb26e92c0744c39c5c47f
Louise Badarani <Louise@louises-macbook-pro.home>
2017-11-02 17:33:51 +0100

    About to test some wierd shit

--- PrepForBosMar/app.R
+++ PrepForBosMar/app.R
@@ -1,35 +1,31 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(ggplot2)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
+  
+  titlePanel("Prep for the next Boston Marathon!"),
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      # Inputs 
+      numericInput("Age", label = h3("Age : "), value = 42),
+      radioButtons("Gender", label = h3("Gender : "),
+                   choices = list("Male" = 1, "Female" = 2), 
+                   selected = NULL),
+      selectInput("Country", label = h3("Country :"), 
+                  choices = c("-", countries_u), 
+                  selected = NULL)
+    ),
+    
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Compare yourself to others", plotOutput("plot")), 
+        tabPanel("Your goal time splits", verbatimTextOutput("summary")), 
+        tabPanel("Yes I do!", tableOutput("table"))
       )
-   )
+    )
+  )
 )
 
 # Define server logic required to draw a histogram
