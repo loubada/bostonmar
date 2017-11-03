324d50029335e9dc49f03aced3971cac170cf662
Louise Badarani <Louise@louises-macbook-pro.home>
2017-11-03 20:17:19 +0100

    First working-ish app

--- PrepForBosMar/app.R
+++ PrepForBosMar/app.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(ggplot2)
 library(shinythemes)
+library(dplyr)
 
 
 ui <- fluidPage( theme = shinytheme('darkly'),
@@ -10,33 +11,45 @@ ui <- fluidPage( theme = shinytheme('darkly'),
   
   br(),
   
+  h4("You think you're ready for the Boston Marathon? Are you sure you're going to make your goal time? Checkout the splits you should aim for and good luck for the race!"),
+  
+  br(),
+  
   sidebarLayout(
     
     sidebarPanel(
-      numericInput("age", label = h4("Age : "), value = 42),
-      radioButtons("gender", label = h3("Gender : "),
-                   choices = list("Male" = 1, "Female" = 2, "N/A" = 3), 
+      h4("Demographics :"),
+      numericInput("age", label = h5("Age : "), value = 42),
+      radioButtons("gender", label = h5("Gender : "),
+                   choices = list("Male" = 1, "Female" = 2, "Don't care!" = 3), 
                    selected = 3),
-      selectInput("country", label = h3("Country :"), 
-                  choices = c("-", "hi", "bye"), 
+      selectInput("country", label = h5("Country :"), 
+                  choices = c("-", countries_u), 
                   selected = NULL),
       
-      br(),
+      hr(),
       
-      h3("Your times (min) :"),
+      h4("Your times (min) :"),
       sliderInput("pastT", label = h5("Past Time : "), min = 120, 
                   max = 520, value = 0),
       sliderInput("goalT", label = h5("Goal Time : "), min = 120, 
-                  max = 520, value = 0)
+                  max = 520, value = 0),
+      
+      hr(),
+      
+      actionButton("lucky", label = "Feeling lucky?")
 
       
     ),
     
     mainPanel(
       tabsetPanel(
-        tabPanel("Compare yourself to others", plotOutput("plot")), 
-        tabPanel("Your goal time splits", tableOutput("table")), 
-        tabPanel("Yes I am!", verbatimTextOutput("fun"))
+        tabPanel("Compare yourself to others", plotOutput("av_plot")), 
+        tabPanel("Your goal time splits", tableOutput("av_table")), 
+        tabPanel("Yes I am!", 
+                 br(),
+                 br(),
+                 TextOutput("fun"))
       )
     )
   )
@@ -49,14 +62,29 @@ ui <- fluidPage( theme = shinytheme('darkly'),
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   output$av_plot <- renderPlot({
+     # input$age
+     # input$gender 
+     # input$country
+     # input$age
+    
    })
+   
+   output$av_table <- renderDataTable({
+     # input$age
+     # input$gender 
+     # input$country
+     # input$age
+     
+   })
+   
+   output$fun <- renderPrint(
+     {
+       cat("In 2017, an 84 runner participated to the race! She also crossed the finished line in 2015 and 2016!")
+       }
+)
+   
+   
 }
 
 # Run the application 
