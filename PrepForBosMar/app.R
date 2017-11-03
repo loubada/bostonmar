137b6e9fa0bc7ba7c598f543e8bf5d3f832cfd10
Louise Badarani <Louise@louises-macbook-pro.home>
2017-11-03 16:41:51 +0100

    Theme is starting to work - ish

--- PrepForBosMar/app.R
+++ PrepForBosMar/app.R
@@ -1,32 +1,49 @@
 library(shiny)
 library(ggplot2)
+library(shinythemes)
 
-ui <- fluidPage(
+
+ui <- fluidPage( theme = shinytheme('darkly'),
+  #shinythemes::themeSelector(),
   
   titlePanel("Prep for the next Boston Marathon!"),
   
   sidebarLayout(
     
     sidebarPanel(
-      # Inputs 
-      numericInput("Age", label = h3("Age : "), value = 42),
-      radioButtons("Gender", label = h3("Gender : "),
-                   choices = list("Male" = 1, "Female" = 2), 
-                   selected = NULL),
-      selectInput("Country", label = h3("Country :"), 
+      numericInput("age", label = h4("Age : "), value = 42),
+      radioButtons("gender", label = h3("Gender : "),
+                   choices = list("Male" = 1, "Female" = 2, "N/A" = 3), 
+                   selected = 3),
+      selectInput("country", label = h3("Country :"), 
                   choices = c("-", countries_u), 
-                  selected = NULL)
+                  selected = NULL),
+      tags$div( class="btn-group open",
+        tags$a( href="#", class="btn btn-default"),
+        tags$a( href="#", class="btn btn-default dropdown-toggle", 'data-toggle' ="dropdown", 'aria-expanded' ="true", tags$span( class="caret")),
+        tags$ul( class="dropdown-menu",
+        tags$li("Action"),
+        tags$li("Another actio"))
+        #<li><a href="#">Another action</a></li>
+        #<li><a href="#">Something else here</a></li>
+       # <li class="divider"></li>
+        #<li><a href="#">Separated link</a></li>
+        )
     ),
     
     mainPanel(
       tabsetPanel(
         tabPanel("Compare yourself to others", plotOutput("plot")), 
-        tabPanel("Your goal time splits", verbatimTextOutput("summary")), 
-        tabPanel("Yes I do!", tableOutput("table"))
+        tabPanel("Your goal time splits", tableOutput("table")), 
+        tabPanel("Yes I do!", verbatimTextOutput("fun"))
       )
     )
   )
+  
+
 )
+  
+     
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
