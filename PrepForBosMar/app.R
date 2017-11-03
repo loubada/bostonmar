01abc568e6cf1847f0342775f29f2241dab70533
Louise Badarani <Louise@louises-macbook-pro.home>
2017-11-03 18:18:31 +0100

    test cycy

--- PrepForBosMar/app.R
+++ PrepForBosMar/app.R
@@ -6,7 +6,9 @@ library(shinythemes)
 ui <- fluidPage( theme = shinytheme('darkly'),
   #shinythemes::themeSelector(),
   
-  titlePanel("Prep for the next Boston Marathon!"),
+  h1("Prep for the next Boston Marathon!"),
+  
+  br(),
   
   sidebarLayout(
     
@@ -16,32 +18,17 @@ ui <- fluidPage( theme = shinytheme('darkly'),
                    choices = list("Male" = 1, "Female" = 2, "N/A" = 3), 
                    selected = 3),
       selectInput("country", label = h3("Country :"), 
-                  choices = c("-", "hi"), 
+                  choices = c("-", "hi", "bye"), 
                   selected = NULL),
       
-      tags$div( class="btn-group", role="group", 'aria-label' ="Button group with nested dropdown", 
-        tags$button( type="button", class="btn btn-primary", color="Primary"),
-        tags$div( class="btn-group", role="group"),
-        tags$button( id="btnGroupDrop1", type="button", 
-                     class="btn btn-primary dropdown-toggle", 'data-toggle'="dropdown", 
-                     'aria-haspopup'="true", 'aria-expanded'="false"),
-        tags$div( class="dropdown-menu", 'aria-labelledby'="btnGroupDrop1", 
-                  'x-placement'="bottom-start", 
-                  style="position: absolute; transform: translate3d(0px, 36px, 0px); top: 0px; left: 0px; will-change: transform;"),
-        tags$a( class="dropdown-item", href="#", "Dropdown link")
-        ),
+      br(),
       
-      tags$div( class="btn-group open",
-        tags$a( href="#", class="btn btn-default"),
-        tags$a( href="#", class="btn btn-default dropdown-toggle", 'data-toggle' ="dropdown", 'aria-expanded' ="true", tags$span( class="caret")),
-        tags$ul( class="dropdown-menu",
-        tags$li("Action"),
-        tags$li("Another actio"))
-        #<li><a href="#">Another action</a></li>
-        #<li><a href="#">Something else here</a></li>
-       # <li class="divider"></li>
-        #<li><a href="#">Separated link</a></li>
-        )
+      h3("Your times (min) :"),
+      sliderInput("pastT", label = h5("Past Time : "), min = 120, 
+                  max = 520, value = 0),
+      sliderInput("goalT", label = h5("Goal Time : "), min = 120, 
+                  max = 520, value = 0)
+
       
     ),
     
@@ -49,7 +36,7 @@ ui <- fluidPage( theme = shinytheme('darkly'),
       tabsetPanel(
         tabPanel("Compare yourself to others", plotOutput("plot")), 
         tabPanel("Your goal time splits", tableOutput("table")), 
-        tabPanel("Yes I do!", verbatimTextOutput("fun"))
+        tabPanel("Yes I am!", verbatimTextOutput("fun"))
       )
     )
   )
