3fb398e6262aebc02fdbfe0e69283121f7fe0d8b
Louise Badarani <Louise@louises-macbook-pro.home>
2017-11-04 21:34:24 +0100

    :(

--- PrepForBosMar/app.R
+++ PrepForBosMar/app.R
@@ -70,35 +70,59 @@ ui <- fluidPage( theme = shinytheme('darkly'),
 
 server <- function(input, output) {
    #Issue: not plotting the graph! It does plot though if we give specific values, maybe an issue with the inputs
-   Gender <- reactive({
-     if (input$Gender == "Male"){
-       "M"
-     }
-     if (input$Gender == "Female"){
-       "F"
-     }
-     else {
-       NULL
-     }
-   })
- 
+  if (input$Gender == "Don't care!") {
+    output$av_plot <- renderPlot({
+      
+      
+      data_to_plot <- demographics_filter(data = data_all, age = input$Age, nationality = input$Country)
+      
+      data_to_plot <- rbind(pasttime(input$pastT, data_to_plot), goaltime(input$goalT, data_to_plot),
+                            top10percentmean(data_to_plot),bottom20percentmean(data_to_plot))
+      
+      ggplot(data = data_to_plot,
+             aes(x = milestone_km, y = mean_time, color = Label)) +
+        geom_point() +
+        geom_smooth() +
+        labs(x = "Distance run", y = "Time since departure")
+      
+    })
+  }
+  else {
+    Gender <- reactive({
+      if (input$Gender == "Male"){
+        "M"
+      }
+      else if (input$Gender == "Female"){
+        "F"
+      }
+      else {
+        NULL
+      }
+    })
     
-  output$av_plot <- renderPlot({
-     
-     
-     data_to_plot <- demographics_filter(data_all, input$Age, Gender(), input$Country)
-     
-     data_to_plot <- rbind(pasttime(input$pastT, data_to_plot), goaltime(input$goalT, data_to_plot),
-                           top10percentmean(data_to_plot),bottom20percentmean(data_to_plot))
-     
-    ggplot(data = data_to_plot,
-                 aes(x = milestone_km, y = mean_time, color = Label)) +
-       geom_point() +
-       geom_smooth() +
-       labs(x = "Distance run", y = "Time since departure")
-
-   })
+    
+    output$av_plot <- renderPlot({
+      
+      
+      data_to_plot <- demographics_filter(data = data_all, age = input$Age, gender = Gender(), nationality = input$Country)
+      
+      data_to_plot <- rbind(pasttime(input$pastT, data_to_plot), goaltime(input$goalT, data_to_plot),
+                            top10percentmean(data_to_plot),bottom20percentmean(data_to_plot))
+      
+      ggplot(data = data_to_plot,
+             aes(x = milestone_km, y = mean_time, color = Label)) +
+        geom_point() +
+        geom_smooth() +
+        labs(x = "Distance run", y = "Time since departure")
+      
+    })
+  }
+  
    
+  
+  ###############################################
+  
+  
    output$av_table <- renderDataTable({
       
    })
