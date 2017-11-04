77db6b71e2ba5d253753ffa92220e0d0321a7df5
Cyriac96 <cyriacparisot@gmail.com>
2017-11-04 14:10:24 +0100

    Creation of the function that plots the average distance vs time for the global population of runners, the top 10% and the bottom 20% runners, using Ruoy's functions

--- Script_Cyriac.R
+++ Script_Cyriac.R
@@ -1,3 +1,23 @@
-#Script de Cyriac #BG
-#or not
+#Script de Cyriac
+
 library(readxl)
+library(ggplot2)
+library(dplyr)
+library (CyCyFns)
+
+#function to plot the output graph in the shiny app
+
+#We apply the functions created by Ruoy on the whole dataset, and we get rid of the 1st 
+#column that is "milestone km", that we already have in global avergage (redundant)
+data_to_plot <- rbind(globalaverage(data_all),
+                      top10percentmean(data_all),bottom20percentmean(data_all))
+
+ggplot(data = (data_to_plot),
+       aes(x = milestone_km, y = mean_time, color = Label)
+) +
+  geom_point() +
+  geom_smooth() +
+  labs(x = "Distance run", y = "Time since departure")
+
+
+
