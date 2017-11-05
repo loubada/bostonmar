f706ddd6b410eb759944ddab27d158ec85549ee1
Louise Badarani <Louise@louises-macbook-pro.home>
2017-11-05 12:24:35 +0100

    table now fully works. Only need to change bar titles.

--- test/app.R
+++ test/app.R
@@ -1,71 +1,119 @@
+# LOUISE
+
 library(shiny)
 library(ggplot2)
 library(shinythemes)
 library(dplyr)
 library (CyCyFns)
 
-ui <- fluidPage( theme = shinytheme('darkly'),
-                 #shinythemes::themeSelector(),`
-                 #push again`
-                 
-                 h1("Prep for the next Boston Marathon!"),
-                 
-                 br(),
-                 
-                 h4("You think you're ready for the Boston Marathon? Are you sure you're going to make your goal time? Checkout the splits you should aim for and good luck for the race!"),
-                 
-                 br(),
-                 
-                 sidebarLayout(
-                   
-                   sidebarPanel(
-                     h4("Demographics :"),
-                     numericInput(inputId = "Age", label = h5("Age : "), value = NULL, min = 18),
-                     radioButtons(inputId = "Gender", label = h5("Gender : "),
-                                  choices = list("Male", "Female", "Don't care!"), 
-                                  selected = "Don't care!", inline = TRUE),
-                     selectInput(inputId = "Country", label = h5("Country :"), 
-                                 choices = c("-", countries_u), 
-                                 selected = "-"),
-                     
-                     hr(),
-                     
-                     h4("Your times (min) :"),
-                     sliderInput(inputId = "pastT", label = h5("Past Time : "), min = 120, 
-                                 max = 520, value = NULL),
-                     sliderInput(inputId = "goalT", label = h5("Goal Time : "), min = 120, 
-                                 max = 520, value = 210),
-                     
-                     hr(),
-                     
-                     actionButton(inputId = "lucky", label = "Feeling lucky?"),
-                     br(),
-                     br(),
-                     h6(textOutput("go")),
-                     
-                     uiOutput("sound")
-                     
-                   ),
-                   
-                   mainPanel(
-                     tabsetPanel(
-                       tabPanel("Compare yourself to others", plotOutput("av_plot")), 
-                       tabPanel("Your goal time splits", tableOutput('av_table')), 
-                       tabPanel("Yes I am!", 
-                                br(),
-                                br(),
-                                textOutput("fun"),
-                                br(),
-                                br(),
-                                h6("Still feeling lucky? Click again!"))
-                       
-                     )
-                   )
-                 )
-                 
-                 
+
+
+########################################################################################################################################  
+
+############################################################        UI      ############################################################ 
+
+########################################################################################################################################
+
+
+
+ui <- fluidPage(
+  theme = shinytheme('darkly'),
+  #shinythemes::themeSelector(),`
+  #push again`
+  
+  h1("Prep for the next Boston Marathon!"),
+  
+  br(),
+  
+  h4(
+    "You think you're ready for the Boston Marathon? Are you sure you're going to make your goal time? Checkout the splits you should aim for and good luck for the race!"
+  ),
+  
+  br(),
+
+###### SIDEBAR
+## INPUTS 
+    
+  sidebarLayout(
+    sidebarPanel(
+      h4("Demographics :"),
+      numericInput(
+        inputId = "Age",
+        label = h5("Age : "),
+        value = NULL,
+        min = 18
+      ),
+      radioButtons(
+        inputId = "Gender",
+        label = h5("Gender : "),
+        choices = list("Male", "Female", "Don't care!"),
+        selected = "Don't care!",
+        inline = TRUE
+      ),
+      selectInput(
+        inputId = "Country",
+        label = h5("Country :"),
+        choices = c("-", countries_u),
+        selected = "-"
+      ),
+      
+      hr(),
+      
+      h4("Your times (min) :"),
+      sliderInput(
+        inputId = "pastT",
+        label = h5("Past Time : "),
+        min = 120,
+        max = 520,
+        value = NULL
+      ),
+      sliderInput(
+        inputId = "goalT",
+        label = h5("Goal Time : "),
+        min = 120,
+        max = 520,
+        value = 210
+      ),
+      
+      hr(),
+      
+      actionButton(inputId = "lucky", label = "Feeling lucky?"),
+      br(),
+      br(),
+      h6(textOutput("go")),
+      
+      uiOutput("sound")
+      
+    ),
+
+###### MAIN
+## OUTPUTS     
+        
+    mainPanel(tabsetPanel(
+      tabPanel("Compare yourself to others", plotOutput("av_plot")),
+      tabPanel("Your goal time splits", tableOutput('av_table')),
+      tabPanel(
+        "Yes I am!",
+        br(),
+        br(),
+        textOutput("fun"),
+        br(),
+        br(),
+        h6("Still feeling lucky? Click again!")
+      )
+      
+    ))
+  )
+  
+  
 )
 
+
+########################################################################################################################################  
+
+##########################################################        SERVER      ########################################################## 
+
+########################################################################################################################################
 
 
 server <- function(input, output) {
@@ -73,10 +121,10 @@ server <- function(input, output) {
   
   
   Gender <- reactive({
-    if (input$Gender == "Male"){
+    if (input$Gender == "Male") {
       "M"
     }
-    else if (input$Gender == "Female"){
+    else if (input$Gender == "Female") {
       "F"
     }
     else {
@@ -86,58 +134,215 @@ server <- function(input, output) {
   
   
   output$av_plot <- renderPlot({
-    
-    if (is.null(Gender())){
-      if (input$Country == "-"){
-        data_to_plot <- demographics_filter(data = data_all, age = input$Age)
+    if (is.null(Gender())) {
+      if (input$Country == "-") {
+        if (is.numeric(input$Age)) {
+          data_to_plot <-
+            demographics_filter(data = data_all, age = input$Age)
+          
+          data_to_plot <-
+            rbind(
+              pasttime(input$pastT, data_to_plot),
+              goaltime(input$goalT, data_to_plot),
+              top10percentmean(data_to_plot),
+              bottom20percentmean(data_to_plot)
+            )
+          
+          ggplot(data = data_to_plot,
+                 aes(
+                   x = milestone_km,
+                   y = mean_time,
+                   color = Label
+                 )) +
+            geom_point() +
+            geom_smooth() +
+            labs(x = "Distance run", y = "Time since departure")
+        }
+        else{
+          data_to_plot <- data_all
+          
+          data_to_plot <-
+            rbind(
+              pasttime(input$pastT, data_to_plot),
+              goaltime(input$goalT, data_to_plot),
+              top10percentmean(data_to_plot),
+              bottom20percentmean(data_to_plot)
+            )
+          
+          ggplot(data = data_to_plot,
+                 aes(
+                   x = milestone_km,
+                   y = mean_time,
+                   color = Label
+                 )) +
+            geom_point() +
+            geom_smooth() +
+            labs(x = "Distance run", y = "Time since departure")
+        }
         
-        data_to_plot <- rbind(pasttime(input$pastT, data_to_plot), goaltime(input$goalT, data_to_plot),
-                              top10percentmean(data_to_plot),bottom20percentmean(data_to_plot))
-        
-        ggplot(data = data_to_plot,
-               aes(x = milestone_km, y = mean_time, color = Label)) +
-          geom_point() +
-          geom_smooth() +
-          labs(x = "Distance run", y = "Time since departure") 
       }
       else {
-        data_to_plot <- demographics_filter(data = data_all, age = input$Age, nationality = input$Country)
+        if (is.numeric(input$Age)){
+          data_to_plot <-
+            demographics_filter(
+              data = data_all,
+              age = input$Age,
+              nationality = input$Country
+            )
+          
+          data_to_plot <-
+            rbind(
+              pasttime(input$pastT, data_to_plot),
+              goaltime(input$goalT, data_to_plot),
+              top10percentmean(data_to_plot),
+              bottom20percentmean(data_to_plot)
+            )
+          
+          ggplot(data = data_to_plot,
+                 aes(
+                   x = milestone_km,
+                   y = mean_time,
+                   color = Label
+                 )) +
+            geom_point() +
+            geom_smooth() +
+            labs(x = "Distance run", y = "Time since departure")
+        }
+        else{
+          data_to_plot <-
+            demographics_filter(
+              data = data_all,
+              nationality = input$Country
+            )
+          
+          data_to_plot <-
+            rbind(
+              pasttime(input$pastT, data_to_plot),
+              goaltime(input$goalT, data_to_plot),
+              top10percentmean(data_to_plot),
+              bottom20percentmean(data_to_plot)
+            )
+          
+          ggplot(data = data_to_plot,
+                 aes(
+                   x = milestone_km,
+                   y = mean_time,
+                   color = Label
+                 )) +
+            geom_point() +
+            geom_smooth() +
+            labs(x = "Distance run", y = "Time since departure")
+        }
         
-        data_to_plot <- rbind(pasttime(input$pastT, data_to_plot), goaltime(input$goalT, data_to_plot),
-                              top10percentmean(data_to_plot),bottom20percentmean(data_to_plot))
-        
-        ggplot(data = data_to_plot,
-               aes(x = milestone_km, y = mean_time, color = Label)) +
-          geom_point() +
-          geom_smooth() +
-          labs(x = "Distance run", y = "Time since departure") 
       }
       
     }
     else {
-      if (input$Country == "-"){
-        data_to_plot <- demographics_filter(data = data_all, age = input$Age, gender = Gender())
+      if (input$Country == "-") {
+        if (is.numeric(input$Age)){
+          data_to_plot <-
+            demographics_filter(data = data_all,
+                                age = input$Age,
+                                gender = Gender())
+          
+          data_to_plot <-
+            rbind(
+              pasttime(input$pastT, data_to_plot),
+              goaltime(input$goalT, data_to_plot),
+              top10percentmean(data_to_plot),
+              bottom20percentmean(data_to_plot)
+            )
+          
+          ggplot(data = data_to_plot,
+                 aes(
+                   x = milestone_km,
+                   y = mean_time,
+                   color = Label
+                 )) +
+            geom_point() +
+            geom_smooth() +
+            labs(x = "Distance run", y = "Time since departure")
+        }
+        else{
+          data_to_plot <-
+            demographics_filter(data = data_all,
+                                gender = Gender())
+          
+          data_to_plot <-
+            rbind(
+              pasttime(input$pastT, data_to_plot),
+              goaltime(input$goalT, data_to_plot),
+              top10percentmean(data_to_plot),
+              bottom20percentmean(data_to_plot)
+            )
+          
+          ggplot(data = data_to_plot,
+                 aes(
+                   x = milestone_km,
+                   y = mean_time,
+                   color = Label
+                 )) +
+            geom_point() +
+            geom_smooth() +
+            labs(x = "Distance run", y = "Time since departure")
+        }
         
-        data_to_plot <- rbind(pasttime(input$pastT, data_to_plot), goaltime(input$goalT, data_to_plot),
-                              top10percentmean(data_to_plot),bottom20percentmean(data_to_plot))
-        
-        ggplot(data = data_to_plot,
-               aes(x = milestone_km, y = mean_time, color = Label)) +
-          geom_point() +
-          geom_smooth() +
-          labs(x = "Distance run", y = "Time since departure") 
       }
       else {
-        data_to_plot <- demographics_filter(data = data_all, age = input$Age, gender = Gender(), nationality = input$Country)
+        if (is.numeric(input$Age)){
+          data_to_plot <-
+            demographics_filter(
+              data = data_all,
+              age = input$Age,
+              gender = Gender(),
+              nationality = input$Country
+            )
+          
+          data_to_plot <-
+            rbind(
+              pasttime(input$pastT, data_to_plot),
+              goaltime(input$goalT, data_to_plot),
+              top10percentmean(data_to_plot),
+              bottom20percentmean(data_to_plot)
+            )
+          
+          ggplot(data = data_to_plot,
+                 aes(
+                   x = milestone_km,
+                   y = mean_time,
+                   color = Label
+                 )) +
+            geom_point() +
+            geom_smooth() +
+            labs(x = "Distance run", y = "Time since departure")
+        }
+        else{
+          data_to_plot <-
+            demographics_filter(
+              data = data_all,
+              gender = Gender(),
+              nationality = input$Country
+            )
+          
+          data_to_plot <-
+            rbind(
+              pasttime(input$pastT, data_to_plot),
+              goaltime(input$goalT, data_to_plot),
+              top10percentmean(data_to_plot),
+              bottom20percentmean(data_to_plot)
+            )
+          
+          ggplot(data = data_to_plot,
+                 aes(
+                   x = milestone_km,
+                   y = mean_time,
+                   color = Label
+                 )) +
+            geom_point() +
+            geom_smooth() +
+            labs(x = "Distance run", y = "Time since departure")
+        }
         
-        data_to_plot <- rbind(pasttime(input$pastT, data_to_plot), goaltime(input$goalT, data_to_plot),
-                              top10percentmean(data_to_plot),bottom20percentmean(data_to_plot))
-        
-        ggplot(data = data_to_plot,
-               aes(x = milestone_km, y = mean_time, color = Label)) +
-          geom_point() +
-          geom_smooth() +
-          labs(x = "Distance run", y = "Time since departure") 
       }
       
       
@@ -150,60 +355,124 @@ server <- function(input, output) {
   
   
   ###############################################
-
+  
   data_for_table <- reactive({
-    if(is.null(Gender())){
-      if(input$Country == "-"){
-        dt <- demographics_filter(data = data_all, age = input$Age)
-        dt <-  goaltime(input$goalT, dt)
-        dt %>% filter(dt$Label == "Goal_time_0to15_mins_Faster") %>% select(mean_time, milestone_km)
+    if (is.null(Gender())) {
+      if (input$Country == "-") {
+        if (is.numeric(input$Age)){
+          dt <- demographics_filter(data = data_all, age = input$Age)
+          dt <-  goaltime(input$goalT, dt)
+          dt %>% filter(dt$Label == "Goal_time_0to15_mins_Faster") %>% select(milestone_km, mean_time)
+        }
+        else{
+          dt <- data_all
+          dt <-  goaltime(input$goalT, dt)
+          dt %>% filter(dt$Label == "Goal_time_0to15_mins_Faster") %>% select(milestone_km, mean_time)
+        }
+        
       }
       else{
-        dt <- demographics_filter(data = data_all, age = input$Age, nationality = input$Country)
-        dt <-  goaltime(input$goalT, dt)
-        dt %>% filter(dt$Label == "Goal_time_0to15_mins_Faster") %>% select(mean_time, milestone_km)
+        if (is.numeric(input$Age)){
+          dt <-
+            demographics_filter(
+              data = data_all,
+              age = input$Age,
+              nationality = input$Country
+            )
+          dt <-  goaltime(input$goalT, dt)
+          dt %>% filter(dt$Label == "Goal_time_0to15_mins_Faster") %>% select(milestone_km, mean_time)
+        }
+        else{
+          dt <-
+            demographics_filter(
+              data = data_all,
+              nationality = input$Country
+            )
+          dt <-  goaltime(input$goalT, dt)
+          dt %>% filter(dt$Label == "Goal_time_0to15_mins_Faster") %>% select(milestone_km, mean_time)
+        }
+        
       }
       
     }
     else{
-      if(input$Country == "-"){
-        dt <- demographics_filter(data = data_all, age = input$Age, gender = Gender())
-        dt <-  goaltime(input$goalT, dt)
-        dt %>% filter(dt$Label == "Goal_time_0to15_mins_Faster") %>% select(mean_time, milestone_km)
+      if (input$Country == "-") {
+        if (is.numeric(input$Age)){
+          dt <-
+            demographics_filter(data = data_all,
+                                age = input$Age,
+                                gender = Gender())
+          dt <-  goaltime(input$goalT, dt)
+          dt %>% filter(dt$Label == "Goal_time_0to15_mins_Faster") %>% select(milestone_km, mean_time)
+        }
+        else{
+          dt <-
+            demographics_filter(data = data_all,
+                                gender = Gender())
+          dt <-  goaltime(input$goalT, dt)
+          dt %>% filter(dt$Label == "Goal_time_0to15_mins_Faster") %>% select(milestone_km, mean_time)
+        }
+        
       }
       else{
-        dt <- demographics_filter(data = data_all, age = input$Age, gender = Gender(), nationality = input$Country)
-        dt <-  goaltime(input$goalT, dt)
-        dt %>% filter(dt$Label == "Goal_time_0to15_mins_Faster") %>% select(mean_time, milestone_km)
+        if (is.numeric(input$Age)){
+          dt <-
+            demographics_filter(
+              data = data_all,
+              age = input$Age,
+              gender = Gender(),
+              nationality = input$Country
+            )
+          dt <-  goaltime(input$goalT, dt)
+          dt %>% filter(dt$Label == "Goal_time_0to15_mins_Faster") %>% select(milestone_km, mean_time)
+        }
+        else{
+          dt <-
+            demographics_filter(
+              data = data_all,
+              gender = Gender(),
+              nationality = input$Country
+            )
+          dt <-  goaltime(input$goalT, dt)
+          dt %>% filter(dt$Label == "Goal_time_0to15_mins_Faster") %>% select(milestone_km, mean_time)
+        }
+        
       }
       
     }
     
     
-    })
-
-  
-  output$av_table <- renderTable(
-    {data_for_table()}
-   
-  )
+  })
   
   
-  ###############################################  
+  output$av_table <- renderTable({
+    data_for_table()
+  })
+  
+  
+  ###############################################
   
   output$fun <- renderPrint({
-    x <- cat("In 2017, an 84 year old runner participated in the race! She also crossed the finished line in 2015 and 2016!")
+    x <-
+      cat(
+        "In 2017, an 84 year old runner participated in the race! She also crossed the finished line in 2015 and 2016!"
+      )
   })
   observeEvent(input$lucky, {
     output$go <- renderPrint({
       cat("Go to the 3rd tab!")
     })
-    output$sound <- renderUI( {tags$audio(src = "Yippy.mp3", type = "audio/mp3", autoplay = NA)})
+    output$sound <-
+      renderUI({
+        tags$audio(src = "Yippy.mp3",
+                   type = "audio/mp3",
+                   autoplay = NA)
+      })
   })
   
   
   
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
